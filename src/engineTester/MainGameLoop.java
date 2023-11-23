968c7a0ef488e5f2f5a8691b524469e537120035
L0rd-Inquisit0r <sean.s.l.duran@gmail.com>
2023-11-23 23:43:48 +0800

    transparency in textures

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,5 +1,9 @@
 package engineTester;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
@@ -24,10 +28,27 @@ public class MainGameLoop {
 		RawModel model=OBJLoader.loadbjModel("res/tree", loader);
 		
 		TexturedModel staticModel=new TexturedModel(model,new ModelTexture(loader.loadTexture("res/tree")));
-		ModelTexture texture=staticModel.getTexture();
 		
-		Entity entity=new Entity(staticModel,new Vector3f(0,0,-25),0,0,0,1);
-		Light light=new Light(new Vector3f(3000,2000,2000),new Vector3f(1,1,1));
+		TexturedModel grass=new TexturedModel(OBJLoader.loadbjModel("res/grassModel", loader),
+				new ModelTexture(loader.loadTexture("res/grassTexture")));
+		grass.getTexture().setHasTransparency(true);
+		grass.getTexture().setUseFakeLighting(true);
+		TexturedModel fern=new TexturedModel(OBJLoader.loadbjModel("res/fern", loader),
+				new ModelTexture(loader.loadTexture("res/fern")));
+		fern.getTexture().setHasTransparency(true);
+		
+		List<Entity> entities=new ArrayList<Entity>();
+		Random random=new Random();
+		for(int i=0;i<500;i++) {
+			entities.add(new Entity(staticModel,new Vector3f(random.nextFloat()*800-400,0,
+					random.nextFloat()*-600),0,0,0,3));
+			entities.add(new Entity(grass,new Vector3f(random.nextFloat()*800-400,0,
+					random.nextFloat()*-600),0,0,0,1));
+			entities.add(new Entity(fern,new Vector3f(random.nextFloat()*800-400,0,
+					random.nextFloat()*-600),0,0,0,0.6f));
+		}
+		
+		Light light=new Light(new Vector3f(20000,20000,20000),new Vector3f(1,1,1));
 		
 		Terrain terrain=new Terrain(-1,-1,loader,new ModelTexture(loader.loadTexture("res/grass")));
 		Terrain terrain2=new Terrain(0,-1,loader,new ModelTexture(loader.loadTexture("res/grass")));
@@ -39,7 +60,9 @@ public class MainGameLoop {
 			camera.move();
 			renderer.processTerrain(terrain);
 			renderer.processTerrain(terrain2);
-			renderer.processEntity(entity);
+			for(Entity entity:entities) {
+				renderer.processEntity(entity);
+			}
 			renderer.render(light, camera);
 			DisplayManager.updateDisplay();
 		}
